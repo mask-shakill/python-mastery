76bbe0fb158041757a38f28f1e35ffdbea741d18
mask-shakill <mask-shakill>
2024-06-06 15:23:44 +0600

    updated

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-def palindrome(n):
-    reversed_n = int(str(n)[::-1])
-    if n == reversed_n:
-        print("Palindrome")
-    else:
-        print("Not Palindrome")
-        
-n = int(input("Enter your Numbers : "))
-palindrome(n)
